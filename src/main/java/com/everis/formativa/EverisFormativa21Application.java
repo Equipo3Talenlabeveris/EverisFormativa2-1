87dcbd8e3ddc462a292bd4734fd5a1b1cd483736
Gabriel Fuentes <nunkmeconecto@gmail.com>
2021-09-15 11:39:53 -0300

    Inicial Commit

---
+++ src/main/java/com/everis/formativa/EverisFormativa21Application.java
@@ -0,0 +1,13 @@
+package com.everis.formativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisFormativa21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisFormativa21Application.class, args);
+	}
+
+}
